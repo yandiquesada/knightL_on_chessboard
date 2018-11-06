51811cb075f3a691334bd5a917d4eb8fba41ab76
Yandi Alonso <yandiquesada@gmail.com>
2018-11-05 23:04:33 -0500

    KNIGHT-Function is capable to find the path to the target.

--- knightL_on_chessboard/Program.cs
+++ knightL_on_chessboard/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace knightL_on_chessboard
 {
@@ -7,19 +8,27 @@ namespace knightL_on_chessboard
     class Knight{
         int sourceXPosition;
         int sourceYPosition;
+        bool wereGeneratedAllPosibleLocationsToGo;
         Knight parent;
         ArrayList posibleLocationsToGo;
         int locationIndex;
+
+        //TODO: this must be a hasset
         Hashtable posibleLocationsToGoKeys;
 
-        public Knight(int sourceXPosition, int sourceYPosition, Knight parent = null)
+        HashSet<string> keyPath;
+
+        public Knight(int sourceXPosition, int sourceYPosition, HashSet<string> keyPath, Knight parent = null)
         {
             this.sourceXPosition = sourceXPosition;
             this.sourceYPosition = sourceYPosition;
+            this.wereGeneratedAllPosibleLocationsToGo = false;
             this.parent = parent;
             this.posibleLocationsToGo = new ArrayList();
             this.posibleLocationsToGoKeys = new Hashtable();
             locationIndex = 0;
+
+            this.keyPath = keyPath;
         }
 
         public int GetSourceXPosition(){
@@ -33,8 +42,12 @@ namespace knightL_on_chessboard
 
         public Knight GetNextLocationToGo()
         {
-            var nextLocation = (Knight)this.posibleLocationsToGo[this.locationIndex];
-            this.locationIndex++;
+            var nextLocation = parent;
+            if (locationIndex < this.posibleLocationsToGo.Count){
+                nextLocation = (Knight)this.posibleLocationsToGo[this.locationIndex];
+                this.locationIndex++;
+            }
+
             return nextLocation;
         }
 
@@ -45,24 +58,22 @@ namespace knightL_on_chessboard
              * 1- no new movements, the parent must be excluded.
              * 2- we found the target!
              */
+            if(wereGeneratedAllPosibleLocationsToGo == true){
+                return this.posibleLocationsToGo;
+            }
 
-
-            //first positive posible pair
             int xDestination = sourceXPosition + xMovements;
             int yDestination = sourceYPosition + yMovements;
             AddPosibleLocationsGo(xDestination, yDestination);
-
-            //first negative posible pair
+            
             xDestination = sourceXPosition - xMovements;
             yDestination = sourceYPosition - yMovements;
             AddPosibleLocationsGo(xDestination, yDestination);
 
-            //second positive posible pair
             xDestination = sourceXPosition + yMovements;
             yDestination = sourceYPosition + xMovements;
             AddPosibleLocationsGo(xDestination, yDestination);
 
-            //second negative posible pair
             xDestination = sourceXPosition - yMovements;
             yDestination = sourceYPosition - xMovements;
             AddPosibleLocationsGo(xDestination, yDestination);
@@ -81,20 +92,22 @@ namespace knightL_on_chessboard
 
             xDestination = sourceXPosition - yMovements;
             yDestination = sourceYPosition + xMovements;
-            AddPosibleLocationsGo(xDestination, yDestination); 
+            AddPosibleLocationsGo(xDestination, yDestination);
 
+            this.wereGeneratedAllPosibleLocationsToGo = true;
             return posibleLocationsToGo;
         }
 
         public void AddPosibleLocationsGo(int xDestination, int yDestination)
         {
-            if (IsValidLocation(xDestination, yDestination) == true && IsParentLocation(xDestination, yDestination) == false)
+            if (IsValidLocation(xDestination, yDestination) == true)
             {
                 String key;
                 key = xDestination.ToString() + yDestination.ToString();
-                if (posibleLocationsToGoKeys.Contains(key) == false)
+                bool isNewLocation = this.keyPath.Add(key);
+                if (posibleLocationsToGoKeys.Contains(key) == false && isNewLocation == true)
                 {
-                    var newLocation = new Knight(xDestination, yDestination, this);
+                    var newLocation = new Knight(xDestination, yDestination, this.keyPath, this);
                     this.posibleLocationsToGoKeys.Add(key, newLocation);
                     this.posibleLocationsToGo.Add(newLocation);
 
@@ -120,8 +133,9 @@ namespace knightL_on_chessboard
             int targetX = 4;
             int targetY = 4;
             bool targetWasFound = false;
+            HashSet<string> keyPath = new HashSet<String>();
 
-            var knightInInitialLocation = new Knight(0, 0);
+            var knightInInitialLocation = new Knight(0, 0, keyPath);
             knightInInitialLocation.GenerateAllPosibleLocationsToGo(1, 2);
             Knight nextLocation = knightInInitialLocation.GetNextLocationToGo();
 
@@ -130,7 +144,7 @@ namespace knightL_on_chessboard
             }
 
             int count = 0;
-            while(targetWasFound == false && count < 20){
+            while(targetWasFound == false && count < 100){
                 Console.WriteLine(nextLocation.GetSourceXPosition().ToString() + nextLocation.GetSourceYPosition().ToString());
 
                 nextLocation.GenerateAllPosibleLocationsToGo(1,2);
@@ -138,16 +152,15 @@ namespace knightL_on_chessboard
                 if (nextLocation.GetSourceXPosition() == targetX && nextLocation.GetSourceYPosition() == targetY)
                 {
                     targetWasFound = true;
+                    Console.WriteLine(nextLocation.GetSourceXPosition().ToString() + nextLocation.GetSourceYPosition().ToString());
                 }
-
                 count++;
             }
-
-
         }
 
         public static void RunLocationGeneration(){
-            var knightInInitialLocation = new Knight(0, 0);
+            HashSet<string> keyPath = new HashSet<String>();
+            var knightInInitialLocation = new Knight(0, 0, keyPath);
             ArrayList posibleLocationsToGo = knightInInitialLocation.GenerateAllPosibleLocationsToGo(1, 2);
             foreach (Knight knight in posibleLocationsToGo)
             {
